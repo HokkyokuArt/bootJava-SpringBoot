16e5357091c4eeb03280d0d12cc49dffa750a736
Jean Artico <jeanartico13@hotmail.com>
2022-07-30 19:35:42 -0300

    Aula 3.3

--- vendas/src/main/java/br/com/tiacademy/vendas/domain/Cliente.java
+++ vendas/src/main/java/br/com/tiacademy/vendas/domain/Cliente.java
@@ -5,36 +5,17 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity
-public class Cliente {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Cliente(Long id, String nome) {
-        this.id = id;
-        this.nome = nome;
-    }
-
-    public Cliente() {
-    }
-
 }
