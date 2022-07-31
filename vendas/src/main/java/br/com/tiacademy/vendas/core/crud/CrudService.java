4ba748b9935174061e0afa56b7b4d7c57ff73793
Jean Artico <jeanartico13@hotmail.com>
2022-07-30 21:43:22 -0300

    Aula 3.4.4

---
+++ vendas/src/main/java/br/com/tiacademy/vendas/core/crud/CrudService.java
@@ -0,0 +1,46 @@
+package br.com.tiacademy.vendas.core.crud;
+
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public abstract class CrudService<T, ID> {
+    @Autowired
+    protected CrudRepository<T, ID> repository;
+
+    public List<T> listar() {
+        return repository.findAll();
+    }
+
+    public T listar_porId(ID id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public T criar(T entidade) {
+        return repository.save(entidade);
+    }
+
+    public void excluir(ID id) {
+        var pessoa = listar_porId(id);
+
+        if (Objects.isNull(pessoa)) {
+            throw new RuntimeException("Não encontrado");
+        }
+        repository.deleteById(id);
+    }
+
+    public T editar(ID id, T entidade) {
+        var recuperado = listar_porId(id);
+        if (Objects.isNull(recuperado)) {
+            throw new RuntimeException("Não encontrado");
+        }
+
+        var entidadeEditada = editarEntidade(recuperado, entidade);
+
+        return repository.save(entidadeEditada);
+    }
+
+    protected abstract T editarEntidade(T recuperado, T entidade);
+
+}
--- vendas/src/main/java/br/com/tiacademy/vendas/service/ClienteService.java
+++ vendas/src/main/java/br/com/tiacademy/vendas/service/ClienteService.java
@@ -1,49 +1,17 @@
 package br.com.tiacademy.vendas.service;
 
-import java.util.List;
-import java.util.Objects;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.tiacademy.vendas.core.crud.CrudService;
 import br.com.tiacademy.vendas.domain.Cliente;
-import br.com.tiacademy.vendas.repository.ClienteRepository;
 
 @Service
-public class ClienteService {
-    @Autowired
-    private ClienteRepository clienteRepository;
+public class ClienteService extends CrudService<Cliente, Long> {
 
-    public List<Cliente> listar_todosClientes() {
-        return clienteRepository.findAll();
+    @Override
+    protected Cliente editarEntidade(Cliente recuperado, Cliente entidade) {
+        recuperado.setNome(entidade.getNome());
+        return recuperado;
     }
 
-    public Cliente listar_clientePorId(Long id) {
-        return clienteRepository.findById(id).orElse(null);
-    }
-
-    public Cliente criar_cliente(Cliente cliente) {
-        return clienteRepository.save(cliente);
-    }
-
-    public Cliente editar_cliente(Long id, Cliente clienteEditado) {
-        var cliente = listar_clientePorId(id);
-
-        if (Objects.isNull(cliente)) {
-            throw new RuntimeException("Cliente não encontrado");
-        }
-
-        cliente.setNome(clienteEditado.getNome());
-
-        return clienteRepository.save(cliente);
-
-    }
-
-    public String excluir_cliente(Long id) {
-        if (Objects.isNull(listar_clientePorId(id))) {
-            throw new RuntimeException("Cliente não encontrado");
-        }
-        clienteRepository.deleteById(id);
-        return "Cliente " + id + " excluído com sucesso!!!";
-    }
 }
