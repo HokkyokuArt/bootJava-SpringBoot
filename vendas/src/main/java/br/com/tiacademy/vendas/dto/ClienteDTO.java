8173cce3fc564c119f64760cc7170896041828cb
Jean Artico <jeanartico13@hotmail.com>
2022-07-31 20:30:56 -0300

    Aula 4.7.1

--- vendas/src/main/java/br/com/tiacademy/vendas/dto/ClienteDTO.java
+++ vendas/src/main/java/br/com/tiacademy/vendas/dto/ClienteDTO.java
@@ -1,5 +1,7 @@
 package br.com.tiacademy.vendas.dto;
 
+import java.io.Serializable;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class ClienteDTO {
+public class ClienteDTO implements Serializable{
     private Long id;
     private String nome;
 
