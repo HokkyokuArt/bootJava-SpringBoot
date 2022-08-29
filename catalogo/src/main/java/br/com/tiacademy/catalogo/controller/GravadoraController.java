f9d17cb4f7225d8f883b8216fad2fd4ffaf8a823
Jean Artico <jeanartico13@hotmail.com>
2022-08-29 20:53:22 -0300

    Checkpoint 25/07/2022

--- catalogo/src/main/java/br/com/tiacademy/catalogo/controller/GravadoraController.java
+++ catalogo/src/main/java/br/com/tiacademy/catalogo/controller/GravadoraController.java
@@ -1,12 +1,37 @@
 package br.com.tiacademy.catalogo.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.tiacademy.catalogo.dto.ArtistaDTO;
+import br.com.tiacademy.catalogo.dto.GravadoraDTO;
 import br.com.tiacademy.catalogo.entity.Gravadora;
+import lombok.var;
 
 @RestController
 @RequestMapping("/gravadora")
 public class GravadoraController extends ControllerCatalago<Gravadora, Long> {
-    
+
+    @GetMapping("/dto")
+    public ResponseEntity<List<GravadoraDTO>> listaDto(){
+        var gravadoras = repository.findAll();
+        var gravadorasDto = gravadoras.stream().map(gravadora -> new GravadoraDTO(
+                gravadora.getId(),
+                gravadora.getNome(),
+                gravadora.getAno(),
+                gravadora.getCidade(),
+                gravadora.getUf(),
+                gravadora.getArtista().stream()
+                    .map(artista -> new ArtistaDTO(artista.getId(), artista.getNome()))
+                    .collect(Collectors.toList())
+            )
+        ).collect(Collectors.toList());
+
+        return ResponseEntity.ok(gravadorasDto);
+    }
 } 
---
+++ catalogo/src/main/java/br/com/tiacademy/catalogo/dto/ArtistaDTO.java
@@ -0,0 +1,15 @@
+package br.com.tiacademy.catalogo.dto;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ArtistaDTO implements Serializable{
+    private Long id;
+    private String nome;
+}
