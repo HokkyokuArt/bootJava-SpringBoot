d551f4c13117abc90c113e6ebec2aea8a76ee345
Jean Artico <jeanartico13@hotmail.com>
2022-10-03 14:09:05 -0300

    Aula 4.8.1

---
+++ vendas/src/main/java/br/com/tiacademy/vendas/domain/Servico.java
@@ -0,0 +1,25 @@
+package br.com.tiacademy.vendas.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import br.com.tiacademy.vendas.core.crud.CrudDomain;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Servico implements CrudDomain<Long>, Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+}
